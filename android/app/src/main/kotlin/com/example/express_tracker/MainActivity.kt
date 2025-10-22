8c55e82c0e18fb15e53d2129593a7f6f4fa9000c
shefattonmoy <shefathossain7@gmail.com>
2025-10-22 16:54:09 +0600

    initialized expense tracker app

---
+++ android/app/src/main/kotlin/com/example/express_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.express_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/expenses.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:express_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(title: 'Flutter Course', amount: 19.99, date: DateTime.now(), category: Category.work),
+    Expense(title: 'Cinema', amount: 15.69, date: DateTime.now(), category: Category.leisure),
+  ];
+  @override
+  Widget build(context) {
+    return Scaffold(body: Column(children: [
+      Text('Chart'),
+      Text('Expenses List')
+    ],
+    ),
+    );
+  }
+}
